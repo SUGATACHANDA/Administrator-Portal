ccdf5718ed95db429d0e20c4c948c842640991bc
SUGATACHANDA <115935348+SUGATACHANDA@users.noreply.github.com>
2024-07-11 22:08:02 +0530

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
@@ -1,7 +1,6 @@
 import { db } from "@/lib/firebase";
 import { Billboards, Category, Size } from "@/types-db";
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { SizeForm } from "./components/size-form";
 
 const BillboardPage = async ({
   params,
@@ -17,7 +16,6 @@ const BillboardPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <SizeForm initialData={size} />
       </div>
     </div>
   );
